bd4b2902570dec6af0078171b94075f25f01e708
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2018-05-18 13:01:12 +0900

    FEAT: implement isSpace', skipSpace' and isEndOfLine'

--- lib/Data/Attoparsec/Applicative.hs
+++ lib/Data/Attoparsec/Applicative.hs
@@ -1,7 +1,10 @@
 module Data.Attoparsec.Applicative
 where
 
+import Prelude hiding (takeWhile)
+import Data.Attoparsec.ByteString.Char8
 import Control.Applicative
+import Control.Monad
 
 (<&&>) :: Applicative f => f Bool -> f Bool -> f Bool
 (<&&>) = liftA2 (&&)
@@ -22,6 +25,18 @@ infix 4 <->
 -- use Template Haskell for much faster implementation?
 inClass' :: String -> Char -> Bool
 inClass' = foldl1 (<||>) . map (==)
+
+-- original isSpace gives True when EOL is input
+isSpace' :: Char -> Bool
+isSpace' = (== ' ') <||> (== '\t')
+
+-- original skipSpace skips EOLs
+skipSpace' :: Parser ()
+skipSpace' = void $ takeWhile isSpace'
+
+-- original isEndOfLine has type Word8 -> Bool
+isEndOfLine' :: Char -> Bool
+isEndOfLine' = (== '\n') <||> (== '\r')
 
 (<:>) :: Applicative f => f a -> f [a] -> f [a]
 (<:>) = liftA2 (:)
